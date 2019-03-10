9f28b3e88eefb8d3ba81b3a1f63e8d539e36b803
Sbe-03 <46492177+Sbe-03@users.noreply.github.com>
2019-03-10 15:36:20 +0530

    Add files via upload

---
+++ VirtualButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbObj;
+    public Animator cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbObj = GameObject.Find("VirtualButton");
+        vbObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cube.GetComponent<Animator>();
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cube.Play("cubeanimation");
+        Application.OpenURL("http://www.facebook.com");
+        Debug.Log("Pressed");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cube.Play("none");
+        Debug.Log("Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ facebook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class facebook : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("virtual1");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        cubeAni.GetComponent<Animator>();
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("cubenimation");
+        Application.OpenURL("http://www.facebook.com");
+        Debug.Log("Pressed");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeAni.Play("none");
+        Debug.Log("Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
